185052faaafb270b72788142c5a902c03aaaa212
Emre <119015914+mredag@users.noreply.github.com>
2023-04-09 19:33:52 +0300

    Update exercise.js
    I'm like a chameleon, always changing... my code, because I'm not sure what I'm doing!

--- es-39/exercise.js
+++ es-39/exercise.js
@@ -4,6 +4,6 @@ function multiplyByTwo(number) {
   }
   return inner;
 }
-
+//// I'm like a chameleon, always changing... my code, because I'm not sure what I'm doing!
 const result = multiplyByTwo(4);
 console.log(result());
